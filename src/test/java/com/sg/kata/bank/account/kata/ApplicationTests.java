edbda6399d5ac0d8bfc3771b49838f8fc94f4b2d
LAANANI <laanani.rami@gmail.com>
2024-12-18 20:54:39 +0100

    Init project

---
+++ src/test/java/com/sg/kata/bank/account/kata/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.kata.bank.account.kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
